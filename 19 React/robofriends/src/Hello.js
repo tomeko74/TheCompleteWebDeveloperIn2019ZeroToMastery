eb63c1f35304aa5ffe287301de44ddbdaba2a9a5
tomeko74 <thomasz.orlowski@gmail.com>
2019-01-20 18:37:53 +0100

    in progress

---
+++ 19 React/robofriends/src/Hello.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+// import logo from './logo.svg';
+// import './App.css';
+
+class Hello extends Component {
+  render() {
+    return (
+        <div>
+            <h1>Hello World</h1>
+            <p>Welcome to React</p>
+        </div>
+    );
+  }
+}
+
+export default Hello;
