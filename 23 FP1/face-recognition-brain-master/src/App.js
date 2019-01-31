9b4cb6f8322520936af3118766df2085aa2d792e
tomeko74 <thomasz.orlowski@gmail.com>
2019-01-31 14:56:04 +0100

    fixes

--- 23 FP1/face-recognition-brain-master/src/App.js
+++ 23 FP1/face-recognition-brain-master/src/App.js
@@ -94,7 +94,7 @@ class App extends Component {
           })
             .then(response => response.json())
             .then(count => {
-              this.setState(Object.assign(this.state.user, { entries: count}))
+              this.setState(Object.assign(this.state.user, {entries: count}))
             })
 
         }
