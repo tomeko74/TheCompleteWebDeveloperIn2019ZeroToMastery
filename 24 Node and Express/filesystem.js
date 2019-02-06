56e7a1534a8d3341bfb5e38ad9426e6cbc354a73
tomeko74 <thomasz.orlowski@gmail.com>
2019-02-06 15:10:03 +0100

    fixes

--- 24 Node and Express/filesystem.js
+++ 24 Node and Express/filesystem.js
@@ -4,5 +4,20 @@ fs.readFile('./hello.txt', (err, data) => {
     if (err) {
         console.log('errrrrrrr');
     }
-    console.log(data.toString('utf8'));
+    console.log('Async', data.toString('utf8'));
+})
+
+const file = fs.readFileSync('./hello.txt');
+console.log('Sync', file.toString('utf8'));
+
+fs.appendFile('./hello.txt', ' This is so cool!', err => {
+    if (err) {
+        console.log(err)
+    }
+})
+
+fs.writeFile('./bad.txt', ' Sad to see you go...', err => {
+    if (err) {
+        console.log(err)
+    }
 })
