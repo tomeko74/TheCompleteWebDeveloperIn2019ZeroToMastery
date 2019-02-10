8e8c9465bf8a47e5189388604b4067aed3d98ff4
tomeko74 <thomasz.orlowski@gmail.com>
2019-02-10 14:56:36 +0100

    section 25 done

--- 23 FP1/face-recognition-brain-master/src/App.js
+++ 23 FP1/face-recognition-brain-master/src/App.js
@@ -83,7 +83,7 @@ class App extends Component {
     this.setState({input: event.target.value});
   }
 
-  onButtonSubmit = () => {
+  onPictureSubmit = () => {
     this.setState({imageUrl: this.state.input});
     app.models
       .predict(
@@ -91,7 +91,7 @@ class App extends Component {
         this.state.input)
       .then(response => {
         if (response) {
-          fetch('http://localhost:3000/image', {
+          fetch('http://localhost:3001/image', {
             method: 'put',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
@@ -135,7 +135,7 @@ class App extends Component {
               />
               <ImageLinkForm
                 onInputChange={this.onInputChange}
-                onButtonSubmit={this.onButtonSubmit}
+                onPictureSubmit={this.onPictureSubmit}
               />
               <FaceRecognition box={box} imageUrl={imageUrl} />
             </div>
