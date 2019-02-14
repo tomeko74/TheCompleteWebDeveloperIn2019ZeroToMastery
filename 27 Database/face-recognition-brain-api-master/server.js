f1d3efa9a4b602060ea0461465e49f463447f6e3
tomeko74 <thomasz.orlowski@gmail.com>
2019-02-14 10:41:34 +0100

    fixes

--- 27 Database/face-recognition-brain-api-master/server.js
+++ 27 Database/face-recognition-brain-api-master/server.js
@@ -74,13 +74,14 @@ app.post('/signin', (req, res) => {
   // } else {
   //   res.status(400).json('error logging in');
   // }
+  const { email, password } = req.body;
   db.select('email', 'hash').from('login')
-    .where('email', '=', req.body.email)
+    .where('email', '=', email)
     .then(data => {
-      const isValid = bcrypt.compareSync(req.body.password, data[0].hash);
+      const isValid = bcrypt.compareSync(password, data[0].hash);
       if (isValid) {
         return db.select('*').from('users')
-          .where('email', '=', req.body.email)
+          .where('email', '=', email)
           .then(user => {
             res.json(user[0])
           })
