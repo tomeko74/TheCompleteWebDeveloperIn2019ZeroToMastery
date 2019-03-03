6d64a0244002b8498b5bb92e833ddbca01d48a05
tomeko74 <thomasz.orlowski@gmail.com>
2019-03-03 12:07:13 +0100

    fixes

--- 19 React/robofriends/src/reducers.js
+++ 19 React/robofriends/src/reducers.js
@@ -3,7 +3,7 @@ import {
   REQUEST_ROBOTS_PENDING,
   REQUEST_ROBOTS_SUCCESS,
   REQUEST_ROBOTS_FAILED
- } from './constants';
+} from './constants';
 
 const initialStateSearch = {
   searchField: ''
