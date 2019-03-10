db9a90828c53e7c8290b3097debd7aaa8ad9fb02
tomeko74 <thomasz.orlowski@gmail.com>
2019-03-10 16:43:37 +0100

    fixes

--- 19 React/robofriends/src/containers/App.js
+++ 19 React/robofriends/src/containers/App.js
@@ -52,6 +52,5 @@ class App extends Component {
     );
   }
 }
-
 // action done from mapDispatchToProps will channge state from mapStateToProps
 export default connect(mapStateToProps, mapDispatchToProps)(App)
